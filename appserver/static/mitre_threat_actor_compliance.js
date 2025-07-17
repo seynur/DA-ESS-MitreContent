865bce8bc257b1e65792fcc1664ed8457812b2b1
Hilal <74376818+hilalgevrek@users.noreply.github.com>
2025-07-18 01:11:45 +0300

    Add threat actor analysis panel to dashboard

---
+++ appserver/static/mitre_threat_actor_compliance.js
@@ -0,0 +1,100 @@
+/* TODO: jink to replace theme_utils with that from core */
+require.config({
+  paths: {
+    theme_utils: '../app/DA-ESS-MitreContent/theme_utils',
+    jquery_local: '../app/DA-ESS-MitreContent/lib/js/jquery-3.6.0.min'
+  }
+});
+
+
+require([
+    'underscore',
+    'jquery_local',
+    'splunkjs/mvc',
+    'splunkjs/mvc/tableview',
+    'theme_utils',
+    'splunkjs/mvc/simplexml/ready!'
+], function(_, jquery, mvc, TableView, themeUtils) {
+  
+    jQuery.noConflict(); // remove jquery conflicts for splunk js sdk
+    var $ = jQuery; // take dollar sign as a local variable
+
+     // Row Coloring Example with custom, client-side range interpretation
+
+    var isDarkTheme = themeUtils.getCurrentTheme && themeUtils.getCurrentTheme() === 'dark';
+
+    var CustomRangeRenderer = TableView.BaseCellRenderer.extend({
+        canRender: function(cell) {
+            //return _(["Initial Access", "Execution", "Persistence", "Privilege Escalation", "Defense Evasion", "Credential Access", "Discovery", "Lateral Movement", "Collection", "Command and Control", "Exfiltration", "Impact", "TA0001:Initial Access", "TA0002:Execution", "TA0003:Persistence", "TA0004:Privilege Escalation", "TA0005:Defense Evasion", "TA0006:Credential Access", "TA0007:Discovery", "TA0008:Lateral Movement", "TA0009:Collection", "TA0011:Command and Control", "TA0010:Exfiltration", "TA0040:Impact","TA0001","TA0002","TA0003","TA0004","TA0005","TA0006","TA0007","TA0008","TA0009","TA0011","TA0010","TA0040"]).contains(cell.field);
+            return _(["SIEM All Rules Techniques", "admin@338", "Agrius", "Ajax Security Team"]).contains(cell.field);
+        },
+        render: function($td, cell) {
+            // Add a class to the cell based on the returned value
+            var value_arr = cell.value.split("|");
+            var technique_id = value_arr[0];
+            var technique_name = value_arr[1];
+            var percentage = value_arr[2];
+            var enabled_count = value_arr[3];
+            var total_count = value_arr[4];
+            var urgency_str = "None"
+
+            var ttl = "Total: " + total_count + "\nEnabled: " + enabled_count + "\nPercentage: " + percentage;
+	          if(technique_id.includes('.')) {
+            	$td.addClass('subtechnique');
+            }
+            else {
+              if(technique_name=="NULL"){
+                $td.addClass('empty');
+                }
+              else {
+                $td.addClass('technique');
+                }
+            }
+            $td.tooltip();
+            $td.prop('title', ttl);
+
+            if (percentage != "NULL") {
+                percentage =  parseFloat(percentage);
+                if(percentage > 70){
+                    $td.addClass('range-cell').addClass('range-compliance_high');
+                }
+	              if(percentage > 50){
+                    $td.addClass('range-cell').addClass('range-compliance_mid');
+                }
+                if(percentage > 30){
+                    $td.addClass('range-cell').addClass('range-compliance_low');
+                }
+                if(percentage >= 0){
+                    $td.addClass('range-cell').addClass('range-compliance_zero');
+                }
+
+            }
+            else if(percentage == "NULL"){
+                $td.addClass('range-cell').addClass('range-none');
+            }
+
+
+            if (isDarkTheme) {
+              $td.addClass('dark');
+            }
+
+            // Update the cell content
+            //$td.text(value.toFixed(2)).addClass('numeric');
+
+            if (technique_name=="NULL"){
+                $td.text(" ");
+            }
+            else {
+                $td.text(technique_id + ": " + technique_name);
+                $td.addClass('add-border').addClass('text-align-center');
+
+            }
+        }
+    });
+
+    mvc.Components.get('mitrematrix').getVisualization(function(tableView) {
+        // Add custom cell renderer, the table will re-render automatically.
+        tableView.addCellRenderer(new CustomRangeRenderer());
+    });
+
+});
---
+++ appserver/static/mitre_threat_actor_triggered.js
@@ -0,0 +1,154 @@
+/* TODO: jink to replace theme_utils with that from core */
+require.config({
+  paths: {
+    theme_utils: '../app/DA-ESS-MitreContent/theme_utils',
+    jquery_local: '../app/DA-ESS-MitreContent/lib/js/jquery-3.6.0.min'
+  }
+});
+
+
+require([
+    'underscore',
+    'jquery_local',
+    'splunkjs/mvc',
+    'splunkjs/mvc/tableview',
+    'theme_utils',
+    'splunkjs/mvc/simplexml/ready!'
+], function(_, $, mvc, TableView, themeUtils) {
+
+    jQuery.noConflict(); // remove jquery conflicts for splunk js sdk
+    var $ = jQuery; // take dollar sign as a local variable
+
+     // Row Coloring Example with custom, client-side range interpretation
+
+    var isDarkTheme = themeUtils.getCurrentTheme && themeUtils.getCurrentTheme() === 'dark';
+
+    var MitreMatrixRenderer = TableView.BaseCellRenderer.extend({
+        canRender: function(cell) {
+            // Enable this custom cell renderer for both the active_hist_searches and the active_realtime_searches field
+	    return _(["SIEM All Rules Techniques", "admin@338", "Agrius", "Ajax Security Team"]).contains(cell.field);
+        },
+        render: function($td, cell) {
+            // Add a class to the cell based on the returned value
+            var value_arr = cell.value.split("|");
+            var technique_id = value_arr[0];
+            var technique_name = value_arr[1];
+            var triggered_count = value_arr[2];
+            var rule_name = value_arr[3];
+            var max_urgency = value_arr[4];
+	    var urgency_str = "None"
+      if(technique_id.includes('.')) {
+		$td.addClass('subtechnique');
+	    }
+      else {
+        if(technique_name=="NULL"){
+          $td.addClass('empty');
+        }
+        else {
+          $td.addClass('technique');
+        }
+      }
+
+	    if (max_urgency==1){
+                urgency_str = "Info";
+		$td.addClass('range-cell').addClass('range-info');
+            }
+            else if (max_urgency==2){
+                urgency_str = "Low";
+		$td.addClass('range-cell').addClass('range-low');
+            }
+            else if (max_urgency==3){
+                urgency_str = "Medium";
+		$td.addClass('range-cell').addClass('range-med');
+            }
+            else if (max_urgency==4){
+                urgency_str = "High";
+		$td.addClass('range-cell').addClass('range-high');
+            }
+            else if (max_urgency==5){
+                urgency_str = "Critical";
+		$td.addClass('range-cell').addClass('range-crit');
+            }
+	    else {
+		$td.addClass('range-cell').addClass('range-none-matrix');
+	    }
+
+            ttl = "Found " + triggered_count + " attacks.\nUrgency: " + urgency_str;
+            $td.tooltip();
+            $td.prop('title', ttl);
+
+
+            if (isDarkTheme) {
+              $td.addClass('dark');
+            }
+
+            if (technique_name=="NULL"){
+                $td.text(" ");
+            }
+            else {
+                $td.text(technique_id + ": " + technique_name);
+                $td.addClass('add-border').addClass('text-align-center');
+
+        }
+        }
+    });
+
+    var MitreTitleRenderer = TableView.BaseCellRenderer.extend({
+        canRender: function(cell) {
+            // Enable this custom cell renderer for both the active_hist_searches and the active_realtime_searches field
+	    return _(["SIEM All Rules Techniques", "admin@338", "Agrius", "Ajax Security Team"]).contains(cell.field);
+        },
+        render: function($td, cell) {
+            // Add a class to the cell based on the returned value
+            var value_arr = cell.value.split("|");
+            triggered_count = value_arr[0];
+            max_urgency = value_arr[1];
+
+            $td.addClass('add-border').addClass('text-align-center');
+
+	    if (max_urgency==1){
+		$td.addClass('title-range-cell').addClass('title-range-info');
+            }
+            else if (max_urgency==2){
+		$td.addClass('title-range-cell').addClass('title-range-low');
+            }
+            else if (max_urgency==3){
+		$td.addClass('title-range-cell').addClass('title-range-med');
+            }
+            else if (max_urgency==4){
+		$td.addClass('title-range-cell').addClass('title-range-high');
+            }
+            else if (max_urgency==5){
+		$td.addClass('title-range-cell').addClass('title-range-crit');
+            }
+	    else {
+		$td.addClass('title-range-cell').addClass('title-range-none');
+	    }
+
+            if (isDarkTheme) {
+              $td.addClass('dark');
+            }
+
+            // Update the cell content
+            //$td.text(value.toFixed(2)).addClass('numeric');
+
+            if (triggered_count=="NULL"){
+                $td.text("0");
+            }
+            else {
+                $td.text(triggered_count);
+            }
+        }
+    });
+
+    mvc.Components.get('mitrematrix').getVisualization(function(tableView) {
+        // Add custom cell renderer, the table will re-render automatically.
+        tableView.addCellRenderer(new MitreMatrixRenderer());
+    });
+
+    mvc.Components.get('mitretitle').getVisualization(function(tableView) {
+        // Add custom cell renderer, the table will re-render automatically.
+        tableView.addCellRenderer(new MitreTitleRenderer());
+    });
+
+});
